603095c6f1612048c3d04999c28dd79279080b1a
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2024-03-24 01:46:19 +0200

    part 1  project todo list

---
+++ my-app/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputFeild from './components/InputFeild';
+import TodoList from './components/TodoList';
+import { Todo } from './model/model';
+
+const App = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isCompleted: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos)
+
+  return (
+    <div className="App">
+      <span className="heading">tasks</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+      
+    </div>
+  );
+}
+
+export default App;
